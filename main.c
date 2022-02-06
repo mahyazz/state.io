ceb95e2348bfdb6f3abd22985c284ed6bc9f88b7
Mahya <mahya.zarrabi@sharif.edu>
2022-02-06 15:20:04 +0330

    Basic Map

--- main.c
+++ main.c
@@ -1,94 +1,144 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <math.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-const int step = 5;
-void moveCircle(SDL_Keycode key, double* snake_x, double* snake_y) {
-	switch (key) {
-		case SDLK_UP:
-			*snake_y -= step;
-			break;
-		case SDLK_DOWN:
-			*snake_y += step;
-			break;
-		case SDLK_RIGHT:
-			*snake_x += step;
-			break;
-		case SDLK_LEFT:
-			*snake_x -= step;
-			break;
-	}
+int cell_size = 120;
+int grid_width = 5;
+int grid_height = 5;
+int window_width = 600 + 1;
+int window_height = 600 + 1;
+
+void draw_background(SDL_Renderer *renderer, SDL_Color color) {
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+    SDL_RenderClear(renderer);
 }
 
-bool hasEatenFood(double snake_x, double snake_y, double food_x, double food_y, double radius, double rx, double ry) {
-	double distance = sqrt(pow(snake_x - food_x, 2) + pow(snake_y - food_y, 2));
-	return distance < radius + (rx + ry) / 2;
-}
+void draw_grids(SDL_Renderer *renderer, int window_width, int window_height,
+                SDL_Color color) {
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
 
-void changeFoodLocation(double * food_x, double * food_y) {
-	*food_x = rand() % 600 + 100;
-	*food_y = rand() % 400 + 100;
-}
-
-const int EXIT = 12345;
-int handleEvents(double* snake_x, double* snake_y) {
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-    	if (event.type == SDL_QUIT)
-    	    return EXIT;
-    	if (event.type == SDL_KEYDOWN)
-    		moveCircle(event.key.keysym.sym , snake_x, snake_y);
+    for (int x = 0; x < 1 + grid_width * cell_size; x += cell_size) {
+        SDL_RenderDrawLine(renderer, x, 0, x, window_height);
     }
-    return 0;
+
+    for (int y = 0; y < 1 + grid_height * cell_size; y += cell_size) {
+        SDL_RenderDrawLine(renderer, 0, y, window_width, y);
+    }
+}
+
+void fill_cell(SDL_Renderer *renderer, int x, int y, SDL_Color color,
+               Uint32 scolor) {
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+
+    SDL_Rect cell;
+    cell.x = x;
+    cell.y = y;
+    cell.w = cell_size;
+    cell.h = cell_size;
+
+    SDL_RenderFillRect(renderer, &cell);
+
+    filledCircleColor(renderer, x + 60, y + 60, 20, scolor);
+}
+
+void draw_map(SDL_Renderer *renderer) {
+    // colors
+    SDL_Color background = {233, 233, 233, 255};       // off-white
+    SDL_Color grid_line_color = {200, 200, 200, 255};  // gray
+    SDL_Color free_land_color = {108, 122, 137, 1};    // blue grey
+    SDL_Color player_a_color = {217, 30, 24, 1};       // red
+    SDL_Color player_b_color = {38, 166, 91, 1};       // green
+    SDL_Color player_c_color = {0, 0, 255, 255};       // blue
+
+    Uint32 free_land_scolor = 0xFF1E1800;  // black
+    Uint32 player_a_scolor = 0xFF00008B;   // dark red
+    Uint32 player_b_scolor = 0xFF008000;   // dark green
+    Uint32 player_c_scolor = 0xFF800000;   // dark blue
+
+    // background
+    draw_background(renderer, background);
+
+    // player land
+    fill_cell(renderer, 0 * cell_size, 0 * cell_size, player_a_color,
+              player_a_scolor);
+    fill_cell(renderer, 2 * cell_size, 2 * cell_size, player_b_color,
+              player_b_scolor);
+    fill_cell(renderer, 1 * cell_size, 4 * cell_size, player_c_color,
+              player_c_scolor);
+
+    // free land
+    fill_cell(renderer, 1 * cell_size, 0 * cell_size, free_land_color,
+              free_land_scolor);
+    fill_cell(renderer, 0 * cell_size, 1 * cell_size, free_land_color,
+              free_land_scolor);
+    fill_cell(renderer, 1 * cell_size, 1 * cell_size, free_land_color,
+              free_land_scolor);
+    fill_cell(renderer, 2 * cell_size, 1 * cell_size, free_land_color,
+              free_land_scolor);
+    fill_cell(renderer, 0 * cell_size, 2 * cell_size, free_land_color,
+              free_land_scolor);
+    fill_cell(renderer, 1 * cell_size, 2 * cell_size, free_land_color,
+              free_land_scolor);
+    fill_cell(renderer, 4 * cell_size, 2 * cell_size, free_land_color,
+              free_land_scolor);
+    fill_cell(renderer, 0 * cell_size, 3 * cell_size, free_land_color,
+              free_land_scolor);
+    fill_cell(renderer, 1 * cell_size, 3 * cell_size, free_land_color,
+              free_land_scolor);
+    fill_cell(renderer, 4 * cell_size, 3 * cell_size, free_land_color,
+              free_land_scolor);
+
+    // grid
+    draw_grids(renderer, window_width, window_height, grid_line_color);
+
+    // render
+    SDL_RenderPresent(renderer);
 }
 
 int main() {
-	double snake_x = 100;
-	double snake_y = 100;
-	double snake_radius = 20;
-    int snake_score = 0;
-
-	double food_x = 300;
-	double food_y = 300;
-	double food_rx = 15;
-	double food_ry = 10;
-
-	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Window* window = SDL_CreateWindow("workshop", 20, 20, 800, 600, SDL_WINDOW_OPENGL);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-    int begining_of_time = SDL_GetTicks();
-    const double FPS = 30;
-    while (1) {
-        int start_ticks = SDL_GetTicks();
-
-        if (handleEvents(&snake_x, &snake_y) == EXIT) break;
-
-    	SDL_SetRenderDrawColor(renderer, 120, 60, 80, 255);
-    	SDL_RenderClear(renderer);
-
-    	if (hasEatenFood(snake_x, snake_y, food_x, food_y, snake_radius, food_rx, food_ry)) {
-    	   	snake_radius *= 1.2;
-            snake_score++;
-    	   	changeFoodLocation(&food_x, &food_y);
-    	}
-
-    	filledCircleRGBA(renderer, snake_x, snake_y, snake_radius, 0, 100, 100, 255);
-    	filledEllipseRGBA(renderer, food_x, food_y, food_rx, food_ry, rand() % 255, rand() % 255, rand() % 255, 255);
-        char* buffer = malloc(sizeof(char) * 50);
-	sprintf(buffer, "score: %d   elapsed time: %dms", snake_score, start_ticks - begining_of_time);
-        /* printf("%s", buffer); */
-        stringRGBA(renderer, 5, 5, buffer, 0, 0, 0, 255);
-    	SDL_RenderPresent(renderer);
-
-        while (SDL_GetTicks() - start_ticks < 1000 / FPS);
+    // init
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Initialize SDL: %s",
+                     SDL_GetError());
+        return EXIT_FAILURE;
     }
 
+    // create window and renderer
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    if (SDL_CreateWindowAndRenderer(window_width, window_height, 0, &window,
+                                    &renderer) < 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                     "Create window and renderer: %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    SDL_SetWindowTitle(window, "state.io");
+
+    // draw map
+    draw_map(renderer);
+
+    // event handling
+    SDL_bool quit = SDL_FALSE;
+
+    while (!quit) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    quit = SDL_TRUE;
+                    break;
+            }
+        }
+    }
+
+    // quitting
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-    return 0;
+
+    return EXIT_SUCCESS;
 }
