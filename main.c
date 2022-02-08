7d91d5dc71a9acfd9066e69a4be42f0d3c62fbc7
Mahya <mahya.zarrabi@sharif.edu>
2022-02-09 02:32:26 +0330

    Update Map

--- main.c
+++ main.c
@@ -11,25 +11,27 @@
 int grid_width = 5;
 int grid_height = 5;
 int window_width = 600 + 1;
-int window_height = 600 + 1;
-const int FPS = 60;
 
 Uint32 colors[][2] = {
     {0xfffaf9f8, 0xfffaf9f8},  // background - off-white
     {0xffe6e2de, 0xffbdb5ad},  // free - grey
-    {0xffa1dfc0, 0xff90c49f},  // player 1 - green
-    {0xfff4e8ad, 0xffecda78},  // player 2 - blue
-    {0xabababff, 0x696969ff},  // player 3 - pink
-    {0xff99eeff, 0xff4ce1ff},  // player 4 - yellow
-    {0xffe5b7d2, 0xffe09ec1},  // player 4 - purple
+    {0xffa1dfc0, 0xff90c49f},  // player 2 - green
+    {0xfff4e8ad, 0xffecda78},  // player 3 - blue
+    {0xffc6c1ff, 0xffa39bfe},  // player 4 - pink
+    {0xff99eeff, 0xff4ce1ff},  // player 5 - yellow
+    {0xffe5b7d2, 0xffe09ec1},  // potion - purple
 };
 
+int max_players = 5;
+int max_soldiers = 30;
+int map_size = 5;
+
 int map[3][5][5] = {{
-                        {0, 0, 1, 2, 0},
+                        {0, 0, 2, 3, 0},
                         {0, 1, 1, 1, 0},
-                        {1, 3, 1, 1, 0},
+                        {4, 1, 1, 1, 0},
                         {3, 1, 1, 0, 0},
-                        {0, 0, 4, 1, 0},
+                        {0, 2, 1, 4, 0},
                     },
                     {
                         {0, 2, 1, 1, 0},
@@ -49,6 +51,7 @@ int map[3][5][5] = {{
 int board[5][5];
 int soldiers[5][5];
 SDL_Renderer *renderer;
+SDL_Surface *surface;
 
 // ---------------------------- Map -----------------------------
 
@@ -60,13 +63,19 @@ void set_board(int k) {
     }
 }
 
-void fill_cell(int x, int y, Uint32 color, Uint32 scolor) {
-    x *= cell_size;
-    y *= cell_size;
+void fill_cell(int i, int j, Uint32 color, Uint32 ccolor) {
+    int x = j * cell_size;
+    int y = i * cell_size;
     boxColor(renderer, x, y, x + cell_size, y + cell_size, color);
-    rectangleColor(renderer, x, y, x + cell_size, y + cell_size, scolor);
-    // filledCircleColor(renderer, x + (cell_size / 2), y + (cell_size / 2),
-    //                   cell_size / 6, scolor);
+    rectangleColor(renderer, x, y, x + cell_size, y + cell_size, ccolor);
+    filledCircleColor(renderer, x + (cell_size / 2), y + (cell_size / 2),
+                      cell_size / 7, ccolor & 0x55ffffff);
+    if (board[i][j]) {
+        char str[10];
+        sprintf(str, "%d", soldiers[i][j]);
+        stringColor(renderer, x + (cell_size / 2) - strlen(str) / 2 * 8,
+                    y + (cell_size / 2) + 2 * sol_radius, str, 0x88000000);
+    }
 }
 
 void draw_map() {
@@ -78,9 +87,38 @@ void draw_map() {
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
             int type = board[i][j];
-            fill_cell(j, i, colors[type][0], colors[type][1]);
+            fill_cell(i, j, colors[type][0], colors[type][1]);
         }
     }
+}
+
+void update_board() {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            soldiers[i][j] = 0;
+            if (board[i][j]) {
+                for (int k = 0; k < listsize; k++) {
+                    if (is_inside(k, i, j, 1) && !is_moving(k) &&
+                        list[k].owner) {
+                        soldiers[i][j]++;
+                        board[i][j] = list[k].owner;
+                    }
+                }
+            }
+        }
+    }
+}
+
+void trench_animation(int i, int j) {
+    int center = cell_size / 2;
+    int sradius = cell_size / 7, bradius = cell_size / 6;
+    int type = board[i][j];
+    filledCircleColor(renderer, i + center, j + center, bradius,
+                      colors[type][1]);
+    filledCircleColor(renderer, i + center, j + center, sradius,
+                      colors[type][1]);
+    filledCircleColor(renderer, i + center, j + center, bradius,
+                      colors[type][1]);
 }
 
 // ---------------------------- Soldiers -----------------------------
@@ -97,13 +135,23 @@ void init_soldiers() {
     }
 }
 
+void generate_soldiers() {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (board[i][j] > 1 && soldiers[i][j] < max_soldiers) {
+                add_soldier(board[i][j], i, j);
+                soldiers[i][j]++;
+            }
+        }
+    }
+}
+
 void show_soldiers() {
     for (int k = 0; k < listsize; k++) {
         Soldier *sol = &list[k];
         Uint32 color = colors[sol->owner][1];
-        int rsoldier = 8;
         if (sol->owner) {
-            filledCircleColor(renderer, sol->x, sol->y, rsoldier, color);
+            filledCircleColor(renderer, sol->x, sol->y, sol_radius, color);
         }
     }
 }
@@ -116,6 +164,41 @@ void move_soldiers() {
         }
     }
 }
+
+void move_to_target(int i, int j, int i2, int j2) {
+    printf("%d:%d -> %d:%d\n", i, j, i2, j2);
+    fflush(stdout);
+    if (!board[i2][j2]) return;
+    if (board[i][j] < 2) return;
+    int delay = 0;
+    for (int k = 0; k < listsize; k++) {
+        Soldier *sol = &list[k];
+        if (sol->i == i && sol->j == j && !is_moving(k)) {
+            set_soldier_target(k, i2, j2, delay);
+            delay += leave_delay;
+        }
+    }
+}
+
+void random_move() {
+    if (rand() % (rand() % (3 * FPS))) return;
+    int i = rand() % map_size;
+    int j = rand() % map_size;
+    int i2 = rand() % map_size;
+    int j2 = rand() % map_size;
+    if (board[i][j] > 2) {
+        move_to_target(i, j, i2, j2);
+    }
+}
+
+// ---------------------------- Potions -----------------------------
+
+// void drop_potion() {
+//     do{
+//      int x = rand() % map_size;
+//     int y = rand() % map_size;
+//     } while (board[x][y] )
+// }
 
 // ---------------------------- Main -----------------------------
 
@@ -130,24 +213,24 @@ int handle_events() {
                 break;
             case SDL_MOUSEBUTTONDOWN:
                 SDL_GetMouseState(&x1, &y1);
-                printf("%d:%d\n", x1, y1);
-                fflush(stdout);
-            case SDL_MOUSEBUTTONUP:
-                SDL_GetMouseState(&x2, &y2);
-                printf("%d:%d\n", x2, y2);
-                fflush(stdout);
                 int i = floor(y1 / cell_size);
                 int j = floor(x1 / cell_size);
+                printf("soldiers[%d][%d]: %d\n", i, j, soldiers[i][j]);
+                // printf("%d:%d\n", x1, y1);
+                fflush(stdout);
+                break;
+            case SDL_MOUSEBUTTONUP:
+                SDL_GetMouseState(&x2, &y2);
+                // printf("%d:%d\n", x2, y2);
+                // fflush(stdout);
+                i = floor(y1 / cell_size);
+                j = floor(x1 / cell_size);
                 int i2 = floor(y2 / cell_size);
                 int j2 = floor(x2 / cell_size);
-                int delay = 0;
-                for (int k = 0; k < listsize; k++) {
-                    Soldier *sol = &list[k];
-                    if (sol->i == i && sol->j == j) {
-                        set_soldier_target(k, i2, j2, delay);
-                        delay += 10;
-                    }
+                if (board[i][j] == 2) {
+                    move_to_target(i, j, i2, j2);
                 }
+                break;
         }
     }
     return 0;
@@ -163,8 +246,9 @@ int main() {
 
     // create window and renderer
     SDL_Window *window;
+    SDL_Texture *texture = NULL;
 
-    if (SDL_CreateWindowAndRenderer(window_width, window_height, 0, &window,
+    if (SDL_CreateWindowAndRenderer(window_width, window_width, 0, &window,
                                     &renderer) < 0) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
                      "Create window and renderer: %s", SDL_GetError());
@@ -177,13 +261,20 @@ int main() {
     set_board(0);
     init_soldiers();
 
+    printf("%d %f\n", listsize, list[0].x);
+
+    int tik = 0;
     while (1) {
         if (handle_events()) {
             break;
         }
 
+        update_board();
+        if (tik++ % (FPS / 1) == 0) {
+            generate_soldiers();
+        }
         draw_map();
-
+        random_move();
         move_soldiers();
         show_soldiers();
 
@@ -192,8 +283,10 @@ int main() {
     }
 
     // cleanup
-    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_FreeSurface(surface);
+    SDL_DestroyTexture(texture);
     SDL_Quit();
 
     return EXIT_SUCCESS;
--- soldier_list.c
+++ soldier_list.c
@@ -11,21 +11,30 @@ typedef struct {
 
 const int steps = 100;
 int cell_size = 120;
-int speed = 3;
-int eps = 3;
+int sol_radius = 7;
+int eps = 10;
+int speed = 2;
+int leave_delay = 12;
+const int FPS = 90;
 
 // ---------------------------- Soldier List -----------------------------
 
-Soldier list[1500];
+Soldier list[2500];
 int listsize = 0;
+
+void set_locatoin(int k, int i, int j) {
+    Soldier *sol = &list[k];
+    sol->x = (j + 0.5) * cell_size;
+    sol->y = (i + 0.5) * cell_size;
+}
 
 void init_soldier(int k, int owner, int i, int j) {
     Soldier *sol = &list[k];
+    // printf("%d", owner);
     sol->owner = owner;
     sol->i = i;
     sol->j = j;
-    sol->x = (j + 0.5) * cell_size;
-    sol->y = (i + 0.5) * cell_size;
+    set_locatoin(k, i, j);
     sol->dx = 0;
     sol->dy = 0;
     sol->delay = 0;
@@ -47,6 +56,14 @@ void add_soldier(int owner, int i, int j) {
 
 void remove_soldier(int k) { list[k].owner = 0; }
 
+int is_moving(int k) { return list[k].dx || list[k].dy; }
+
+int is_inside(int k, int i, int j, float r) {
+    Soldier *sol = &list[k];
+    return (fabs(sol->x - (j + 0.5) * cell_size) < r &&
+            fabs(sol->y - (i + 0.5) * cell_size) < r);
+}
+
 void set_soldier_target(int k, int i, int j, int delay) {
     Soldier *sol = &list[k];
     float angle = atan2(i - sol->i, j - sol->j);
@@ -57,25 +74,36 @@ void set_soldier_target(int k, int i, int j, int delay) {
     sol->delay = delay;
 }
 
+void manage_conflict(int k) {
+    Soldier *sol = &list[k];
+    for (int k2 = 0; k2 < listsize; k2++) {
+        Soldier *sol2 = &list[k2];
+        if ((sol->owner != sol2->owner) && sol2->owner &&
+            (is_moving(k2) || (sol->i == sol2->i && sol->j == sol2->j)) &&
+            fabs(sol->x - sol2->x) < eps && fabs(sol->y - sol2->y) < eps) {
+            remove_soldier(k);
+            remove_soldier(k2);
+            // if (list[k].dx == 0 || list[k].dy == 0) {
+            //     trench_animation(list[k].dx, list[k].dy);
+            // }
+            // if (list[k2].dx == 0 || list[k2].dy == 0) {
+            //     trench_animation(list[k2].dx, list[k2].dy);
+            // }
+            break;
+        }
+    }
+}
+
 void move_soldier(int k) {
     Soldier *sol = &list[k];
     if (sol->delay > 0) {
         sol->delay--;
     }
-    // check conflicts and kill
-    for (int k2 = 0; k2 < listsize; k2++) {
-        Soldier *sol2 = &list[k2];
-        if ((sol->owner != sol2->owner) && sol2->owner &&
-            fabs(sol->x - sol2->x) < eps && fabs(sol->y - sol2->y) < eps) {
-            remove_soldier(k);
-            remove_soldier(k2);
-            break;
-        }
-    }
-    if (fabs(sol->x - (sol->j + 0.5) * cell_size) < eps &&
-        fabs(sol->y - (sol->i + 0.5) * cell_size) < eps) {
+    manage_conflict(k);
+    if (is_inside(k, sol->i, sol->j, eps)) {
         sol->dx = 0;
         sol->dy = 0;
+        set_locatoin(k, sol->i, sol->j);
     }
     if (sol->delay == 0) {
         sol->x += sol->dx;
